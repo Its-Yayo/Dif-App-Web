027ced983013e4a80faa8fabb4aa17a938fcf38b
Its-Yayo <a01754574@itesm.mx>
2023-10-09 10:17:46 -0600

    Add TODO Implementation #6

--- app/routes.py
+++ app/routes.py
@@ -39,3 +39,34 @@ def login() -> Response | Any:
 def tablero() -> None:
     render_template('tablero.html')
 
+@main.route("/tablero/afluencia/<string:idComedor>", methods=['GET'])
+def tablero_afluencia(idComedor: int) -> Response:
+    ...
+
+@main.route("/tablero/recaudacion", methods=['GET'])
+def tablero_recaudacion() -> Response:
+    ...
+
+@main.route("/tablero/afluencia_año", methods=['GET'])
+def tablero_afluencia_year() -> Response:
+    ...
+
+@main.route("/tablero/afluencia_comedores", methods=['GET'])
+def tablero_comedores() -> Response:
+    ...
+
+@main.route("/afluencia", methods=['GET'])
+def afluencia() -> None:
+    ...
+
+@main.route("/afluencia/<idComedor>/<condicion>", methods=['POST', 'GET'])
+def afluencia_registros(idComedor: int, condicion: str) -> Response:
+    ...
+
+@main.route("/afluencia/inscritos/<idComedor>", methods=['GET'])
+def afluencia_inscritos(idComedor: int) -> Response:
+    ...
+
+@main.route("/afluencia/predicciones/idComedor>", methods=['GET'])
+def afluencia_predicciones(idComedor: int) -> Response:
+    ...
