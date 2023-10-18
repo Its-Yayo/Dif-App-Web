86ded3e00613d7ccba89b724faa9beaccec289f6
Its-Yayo <a01754574@itesm.mx>
2023-10-17 23:36:35 -0600

    Add Usages #16 FIXME

---
+++ app/static/js/script_donaciones.js
@@ -0,0 +1,20 @@
+/* /usr/env/node node */
+
+function actualizarDonacionesTotales() {
+    console.log("La funcion actualizarDonacionesTotales se ha ejecutado");
+    const comedorId = document.getElementById("comedor").value;
+
+    fetch(`/recaudaciones_donaciones?comedor=${comedorId}`)
+        .then(response => response.json())
+        .then(data => {
+            const donacionesSpan = document.getElementById("donaciones-value");
+            donacionesSpan.textContent = data.donaciones_totales;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+actualizarDonacionesTotales();
+
+setInterval(actualizarDonacionesTotales, 10000); // Actualizar cada 10 segundos
---
+++ app/static/js/script_ventas.js
@@ -0,0 +1,20 @@
+/* /usr/env/node node */
+
+function actualizarVentasTotales() {
+    console.log("La funcion actualizarVentasTotales se ha ejecutado");
+    const comedorId = document.getElementById("comedor").value;
+
+    fetch(`/recaudaciones_ventas?comedor=${comedorId}`)
+        .then(response => response.json())
+        .then(data => {
+            const ventasSpan = document.getElementById("ventas-value");
+            ventasSpan.textContent = data.ventas_totales;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+actualizarVentasTotales();
+
+setInterval(actualizarVentasTotales, 10000); // Actualizar cada 10 segundos
